b9558ea6319429dfe1d1e809bf42946136584f3b
majicmaj <majd.tarbin@outlook.com>
2025-06-05 17:16:04 -0400

    fix: remove unnecessary type keyword from Todo import in TodoItem component

--- src/components/todo/todoItem.tsx
+++ src/components/todo/todoItem.tsx
@@ -1,6 +1,6 @@
 import { useCompleteTodo, useUncompleteTodo } from '@/data/todo/todos'
 import { cn } from '@/lib/utils'
-import { type Todo } from '@/types/todo'
+import { Todo } from '@/types/todo'
 
 const TodoItem = ({
   todo,
